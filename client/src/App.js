8ba6a40951c50d84dff71fce3daf81631f5a28cb
Vipin019 <vkpmail019@gmail0com>
2023-06-04 17:46:46 +0530

    Client added

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header/header';
+import Nav from './components/nav/Nav'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+// import Services from './components/services/services'
+import Featured from './components/Featured/Featured'
+import Portfolio from './components/portfolio/portfolio'
+// import Testimonials from './components/testimonials/testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/footer'
+
+const App = () => {
+  return (
+    <div>
+    <Header></Header>
+    <Nav></Nav>
+    <About></About>
+    <Experience></Experience>
+    <Featured></Featured>
+    {/* <Services></Services> */}
+    <Portfolio></Portfolio>
+    {/* <Testimonials></Testimonials> */}
+    <Contact></Contact>
+    <Footer></Footer>
+
+    </div>
+  )
+}
+
+export default App
+
---
+++ client/src/components/nav/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {CgProfile} from 'react-icons/cg'
+import {BsBook} from 'react-icons/bs'
+import {MdMiscellaneousServices} from 'react-icons/md'
+import {MdContactPhone} from 'react-icons/md'
+import {useState} from 'react'
+
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
+  return (
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav==='#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav==='#about' ? 'active' : ''}><CgProfile/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav==='#experience' ? 'active' : ''}><BsBook/></a>
+      <a href="#featured" onClick={() => setActiveNav('#featured')} className={activeNav==='#featured' ? 'active' : ''}><MdMiscellaneousServices/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav==='#contact' ? 'active' : ''}><MdContactPhone/></a>
+     </nav>
+  )
+}
+
+export default Nav
