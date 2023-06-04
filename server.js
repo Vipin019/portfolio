8dcc54e9067d63558494c6ca0ac8bdd7dc18ccc4
Vipin019 <vkpmail019@gmail0com>
2023-06-04 17:38:39 +0530

    Backend added

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+//config dotenv
+dotenv.config();
+
+//es module file dir
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//resapi object
+const app = express();
+
+//middelwares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+//universal route
+app.use("*", function (req, res) {
+  res.sendFile(__dirname, path.join("./client/build/index.html"));
+});
+//PORT
+const PORT = process.env.PORT;
+//server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
