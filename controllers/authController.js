8546b1d2103226ab12cdf9dbd4b4bf74d9ad2bb9
Vipin019 <vkpmail019@gmail0com>
2023-06-05 10:01:03 +0530

    starting

---
+++ controllers/authController.js
@@ -0,0 +1,105 @@
+import userModel from "../models/userModel.js";
+
+import { comparePassword, hashPassword } from "./../helpers/authHelper.js";
+import JWT from "jsonwebtoken";
+
+//register controller
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, idCardNo, education } = req.body;
+    //validations
+    if (!name) {
+      return res.send({ error: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ message: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ message: "Password is Required" });
+    }
+    //check user
+    const exisitingUser = await userModel.findOne({ email });
+    //exisiting user
+    if (exisitingUser) {
+      return res.status(200).send({
+        success: false,
+        message: "Already Register please login",
+      });
+    }
+    //register user
+    const hashedPassword = await hashPassword(password);
+    //save
+    const user = await new userModel({
+      name,
+      email,
+      password: hashedPassword,
+      education,
+      idCardNo,
+    }).save();
+
+    res.status(201).send({
+      success: true,
+      message: "Register Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Errro in Registeration",
+      error,
+    });
+  }
+};
+
+//POST LOGIN
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //validation
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    //check user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Email is not registerd",
+      });
+    }
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+    //token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).send({
+      success: true,
+      message: "login successfully",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        education: user.education,
+        idCardNo: user.idCardNo,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in login",
+      error,
+    });
+  }
+};
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoute.js";
+import connectDB from "./config/db.js";
 
 //config dotenv
 dotenv.config();
@@ -11,6 +13,9 @@ dotenv.config();
 //es module file dir
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+//databse config
+connectDB();
 
 //resapi object
 const app = express();
@@ -22,9 +27,13 @@ app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 //universal route
-app.get("*", function (req, res) {
+app.use("*", function (req, res) {
   res.sendFile(__dirname, path.join("./client/build/index.html"));
 });
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+
 //PORT
 const PORT = process.env.PORT;
 //server
